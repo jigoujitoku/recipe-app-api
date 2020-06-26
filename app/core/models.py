0215d0624efcd61ab269ae2d3bf2d9e1e8b3e117
kakavnered <jigoujitoku@gmail.com>
2020-06-26 12:32:18 +0200

    Updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -9,7 +9,7 @@ class UserManager(BaseUserManager):
         """Creates and saves a new user"""
         if not email:
             raise ValueError('Users must have an email address')
-        user = self.model (email = self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
 
@@ -27,10 +27,10 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff= models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
